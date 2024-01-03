c9f6ebe0beca387d9e2e2345798eb98c816cd0c3
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-01-03 17:14:35 -0300

    feat: add components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,15 @@
-// import Blog from './components/blog';
 import About from './components/about';
+import Blog from './components/blog';
 import Intro from './components/intro';
+import Skills from './components/skills';
 
 export default function HomePage() {
   return (
     <main>
       <Intro />
+      <Blog />
       <About />
+      <Skills />
     </main>
   );
 }
