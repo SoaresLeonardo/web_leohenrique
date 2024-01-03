19c6f82accaa5ec08f6a7dfd957199d8fcbd171c
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-01-03 14:03:12 -0300

    feat(Button): add cursor hook and edit styles

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,11 @@
+'use client';
+
 import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
+import { useCursor } from '@/hooks/useCursor';
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
@@ -14,9 +17,8 @@ const buttonVariants = cva(
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        secondary: 'bg-secondary text-primary hover:bg-secondary/80',
+        ghost: 'hover:bg-accent text-foreground hover:text-accent-foreground',
         link: 'text-foreground underline-offset-4 hover:underline'
       },
       size: {
@@ -42,10 +44,13 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
+    const { setHovered } = useCursor();
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
         {...props}
       />
     );
